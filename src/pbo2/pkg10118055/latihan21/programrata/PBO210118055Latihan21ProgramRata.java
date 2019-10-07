c99ed112dc15b87da95097abc3af6925a9357d9e
User <User@DESKTOP-2RC2UP5>
2019-10-07 20:15:06 +0700

    first commit

---
+++ src/pbo2/pkg10118055/latihan21/programrata/PBO210118055Latihan21ProgramRata.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118055.latihan21.programrata;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class PBO210118055Latihan21ProgramRata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         
+        Scanner userInput = new Scanner (System.in);
+        System.out.println("Masukkan Banyak Mahasiswa:"); 
+        int input = userInput.nextInt();
+        
+      
+    
+        for( int i=0;i<input;i++){
+          
+        System.out.println("nilai mahasiswa ke-"+(i+1)+":");
+            int n1 = userInput.nextInt();
+       
+        
+      
+            
+        }
+        int n1 = 0;
+       
+        
+       int nilai = input/n1;
+       
+       System.out.printf("maka,nilai rata-rata"+nilai);
+       
+      
+    }
+    
+}
